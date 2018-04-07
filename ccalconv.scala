7018e5bdf5a09462993a87ff34af4ac233aaf580
Makoto Tanabe <xekid78@outlook.com>
2018-04-07 21:02:42 +0900

    Add files via upload

--- ccalconv.scala
+++ ccalconv.scala
@@ -1,8 +1,9 @@
 object Ccalconv {
     def main(args: Array[String]): Unit = {
+        var syowa = 0
         for (seireki <- 1926 to 1935) {
             print(s"西暦${seireki}年は、")
-            var syowa = seireki - 1925
+            syowa = seireki - 1925
             println(s"昭和${syowa}年です。")
         }
     }
